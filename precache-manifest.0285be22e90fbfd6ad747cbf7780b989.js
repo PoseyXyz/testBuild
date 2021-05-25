38528fdfead9cc28362bb0d20cd91da9b7931626
Posey <adeposi60@gmail.com>
2021-05-25 15:59:58 +0100

    Updates

---
+++ precache-manifest.0285be22e90fbfd6ad747cbf7780b989.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75040a2d2921ac0fe60ff05398f82bf",
+    "url": "/testBuild/index.html"
+  },
+  {
+    "revision": "d6f0d10b3f05be5c578b",
+    "url": "/testBuild/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3c284f0d189c5100d3f5",
+    "url": "/testBuild/static/css/main.e87e9c5b.chunk.css"
+  },
+  {
+    "revision": "d6f0d10b3f05be5c578b",
+    "url": "/testBuild/static/js/2.60c48fbe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/testBuild/static/js/2.60c48fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c284f0d189c5100d3f5",
+    "url": "/testBuild/static/js/main.3db87212.chunk.js"
+  },
+  {
+    "revision": "9f8ef77f9be320408039",
+    "url": "/testBuild/static/js/runtime-main.bc2266d4.js"
+  },
+  {
+    "revision": "8722fbd0a8eed3a7eb81f59aae3fcfa3",
+    "url": "/testBuild/static/media/45.8722fbd0.gif"
+  }
+]);
